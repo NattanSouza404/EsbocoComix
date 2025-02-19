db6b3adc455782598110655d4fdbfe93eeb984c0
EthanHueh <nattanssouzapvz@gmail.com>
2025-02-19 11:59:19 -0300

    Ajustes nas entidades

---
+++ src/main/java/com/fatec/modelo/entidades/Cartao.java
@@ -0,0 +1,10 @@
+package com.fatec.modelo.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cartao {
+    
+}
--- src/main/java/com/fatec/modelo/entidades/Cliente.java
+++ src/main/java/com/fatec/modelo/entidades/Cliente.java
@@ -1,5 +1,7 @@
 package com.fatec.modelo.entidades;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,4 +10,13 @@ import lombok.Setter;
 public class Cliente {
     private int         id;
     private String      nome;
+    private String genero;
+
+    private List<Telefone> telefones;
+    private List<Cartao> cartoesCliente;
+    private List<Endereco> enderecosCobranca;
+    private List<Endereco> enderecosResidenciais;
+
+    
+
 }
