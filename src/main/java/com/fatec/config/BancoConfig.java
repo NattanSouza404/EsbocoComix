595bd6aa605f65ee808517779d4b63ca315b16df
EthanHueh <nattanssouzapvz@gmail.com>
2025-02-19 15:59:36 -0300

    Adição de arquivo de configuração

--- src/main/java/com/fatec/config/BancoConfig.java
+++ src/main/java/com/fatec/config/BancoConfig.java
@@ -1,8 +1,34 @@
 package com.fatec.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BancoConfig {
-    public static final String DRIVER   = "org.postgresql.Driver";
-    public static final String URL      = "jdbc:postgresql://localhost:5432/projeto_les";
-    public static final String USER     = "postgres";
-    public static final String PASSWORD = "admin123";
+    public static final String DRIVER;
+    public static final String URL;
+    public static final String USER;  
+    public static final String PASSWORD ;
+
+    static {
+
+        Properties properties = new Properties();
+        
+        try (FileInputStream input = new FileInputStream("src/main/resources/config.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DRIVER  = properties.getProperty("database.driver");
+        URL     = properties.getProperty("database.url");
+        USER    = properties.getProperty("database.user");
+        PASSWORD= properties.getProperty("database.password");
+
+        System.out.println("Carregando propriedades do banco de dados");
+        System.out.println("database.url = "+URL);
+        System.out.println("database.driver = "+DRIVER);
+        System.out.println("database.user = "+USER);
+        System.out.println("database.password = "+PASSWORD);
+    }
 }
