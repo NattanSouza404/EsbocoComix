af5dbaa9ec394765ee255598f19ee7c9e215293d
EthanHueh <nattanssouzapvz@gmail.com>
2025-03-04 00:37:41 -0300

    Avanços na modelagem do banco

--- esboco_comix.sql
+++ esboco_comix.sql
@@ -5,7 +5,7 @@ CREATE DATABASE esboco_comix;
 
 -- Criacao da tabela
 CREATE TABLE clientes (
-    cli_id            SERIAL PRIMARY KEY,
+    cli_id            NUMERIC(6) PRIMARY KEY,
     cli_nome          VARCHAR(100) NOT NULL,
     cli_genero        VARCHAR(20) NOT NULL,
     cli_dt_nascimento DATE NOT NULL,
@@ -14,11 +14,66 @@ CREATE TABLE clientes (
     cli_hash_senha    VARCHAR(70) NOT NULL,
     cli_salt_senha    VARCHAR(24) NOT NULL,
     cli_ranking       NUMERIC(6) NOT NULL,
-    cli_is_ativo      BOOLEAN NOT NULL
+    cli_is_ativo      BOOLEAN NOT NULL,
+    cli_tel_id        NUMERIC(6) NOT NULL
 );
 
--- Exemplo de linha
-INSERT INTO cliente
-	(nome)
-VALUES
-	('Jorge');
+CREATE TABLE telefones (
+    tel_id     NUMERIC(6) PRIMARY KEY,
+    tel_ddd    CHAR(2) NOT NULL,
+    tel_numero CHAR(9) NOT NULL,
+    tel_tipo   VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE clientes
+    ADD CONSTRAINT fk_cli_tel FOREIGN KEY (cli_tel_id) REFERENCES telefones(tel_id);
+
+CREATE SEQUENCE cli_sq
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE FUNCTION set_cli_id() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    NEW.cli_id := nextval('cli_sq');
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER trg_set_cli_id
+BEFORE INSERT ON clientes
+FOR EACH ROW
+EXECUTE FUNCTION set_cli_id();
+
+CREATE SEQUENCE tel_sq
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE FUNCTION set_tel_id() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    NEW.tel_id := nextval('tel_sq');
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER tg_set_tel_id
+BEFORE INSERT ON telefones
+FOR EACH ROW
+EXECUTE FUNCTION set_tel_id();
+
+INSERT INTO clientes(
+	cli_nome, cli_genero, cli_dt_nascimento, cli_cpf, cli_email, cli_hash_senha, cli_salt_senha, cli_ranking, cli_is_ativo, cli_tel_id)
+	VALUES ('Jorge', 'A', '2002-02-02', '9999999999', 'a', 'a', 'a', 1, true, 1);
+
+INSERT INTO telefones(
+	tel_id, tel_ddd, tel_numero, tel_tipo)
+	VALUES (3, 'aa', 'aaaaaaaaa', 'a');
--- modelagem_cliente.sql
+++ modelagem_cliente.sql
@@ -1,5 +1,5 @@
 -- Gerado por Oracle SQL Developer Data Modeler 22.2.0.165.1149
---   em:        2025-02-19 12:28:33 BRT
+--   em:        2025-03-01 17:25:45 BRT
 --   site:      Oracle Database 11g
 --   tipo:      Oracle Database 11g
 
@@ -17,202 +17,177 @@ LOGGING;
 
 ALTER TABLE bandeiras_cartao_credito ADD CONSTRAINT pk_bcc PRIMARY KEY ( bcc_id );
 
-CREATE TABLE cartoes_cliente (
-    ctc_cli_id          NUMBER(6) NOT NULL,
-    ctc_cre_id          NUMBER(9) NOT NULL,
-    ctc_is_preferencial CHAR(1) NOT NULL
-)
-LOGGING;
-
-ALTER TABLE cartoes_cliente ADD CONSTRAINT pk_ctc PRIMARY KEY ( ctc_cli_id,
-                                                                ctc_cre_id );
-
 CREATE TABLE cartoes_credito (
     cre_id               NUMBER(9) NOT NULL,
     cre_numero           CHAR(16) NOT NULL,
     cre_nome_impresso    VARCHAR2(100) NOT NULL,
     cre_codigo_seguranca CHAR(3) NOT NULL,
-    cre_bandeira_cartao  NUMBER(2) NOT NULL
+    cre_is_preferencial  CHAR(1) NOT NULL,
+    cre_bcc_id           NUMBER(2) NOT NULL,
+    cre_cli_id           NUMBER(6) NOT NULL
 )
 LOGGING;
 
 ALTER TABLE cartoes_credito ADD CONSTRAINT pk_cre PRIMARY KEY ( cre_id );
 
+CREATE TABLE categorias (
+    cat_id   unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    cat_nome unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+
+)
+LOGGING;
+
+CREATE TABLE categorias_quadrinho (
+    cqu_cat_id unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    cqu_qua_id unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+
+)
+LOGGING;
+
 CREATE TABLE clientes (
     cli_id            NUMBER(6) NOT NULL,
     cli_nome          VARCHAR2(100) NOT NULL,
+    cli_genero        VARCHAR2(20) NOT NULL,
     cli_dt_nascimento DATE NOT NULL,
     cli_cpf           CHAR(11) NOT NULL,
     cli_email         VARCHAR2(100) NOT NULL,
-    cli_senha         VARCHAR2(16) NOT NULL,
-    cli_gen_id        NUMBER(2) NOT NULL,
-    cli_ranking       NUMBER(6)
+    cli_hash_senha    VARCHAR2(70) NOT NULL,
+    cli_salt_senha    VARCHAR2(24) NOT NULL,
+    cli_ranking       NUMBER(6) NOT NULL,
+    cli_is_ativo      CHAR(1) NOT NULL,
+    cli_tel_id        NUMBER(6) NOT NULL
 )
 LOGGING;
 
 ALTER TABLE clientes ADD CONSTRAINT pk_cli PRIMARY KEY ( cli_id );
 
-CREATE TABLE endereco_cobranca (
-    eco_cli_id NUMBER(6) NOT NULL,
-    eco_end_id NUMBER(6) NOT NULL
-)
-LOGGING;
-
-ALTER TABLE endereco_cobranca ADD CONSTRAINT pk_eco PRIMARY KEY ( eco_end_id,
-                                                                  eco_cli_id );
-
-CREATE TABLE endereco_entrega (
-    een_cli_id NUMBER(6) NOT NULL,
-    een_end_id NUMBER(6) NOT NULL
-)
-LOGGING;
-
-ALTER TABLE endereco_entrega ADD CONSTRAINT pk_een PRIMARY KEY ( een_end_id,
-                                                                 een_cli_id );
-
 CREATE TABLE enderecos (
-    end_id          NUMBER(6) NOT NULL,
-    end_logradouro  VARCHAR2(100) NOT NULL,
-    end_tlo_id      NUMBER(2) NOT NULL,
-    end_tre_id      NUMBER(2) NOT NULL,
-    end_numero      NUMBER(3) NOT NULL,
-    end_bairro      VARCHAR2(50) NOT NULL,
-    end_cep         CHAR(8) NOT NULL,
-    end_cidade      VARCHAR2(50) NOT NULL,
-    end_estado      VARCHAR2(20) NOT NULL,
-    end_pais        VARCHAR2(30) NOT NULL,
-    end_observacoes VARCHAR2(50)
+    end_id               NUMBER(6) NOT NULL,
+    end_frase_curta      VARCHAR2(30) NOT NULL,
+    end_logradouro       VARCHAR2(100) NOT NULL,
+    end_tipo_logradouro  VARCHAR2(20) NOT NULL,
+    end_tipo_residencial VARCHAR2(20) NOT NULL,
+    end_numero           NUMBER(3) NOT NULL,
+    end_bairro           VARCHAR2(50) NOT NULL,
+    end_cep              CHAR(8) NOT NULL,
+    end_cidade           VARCHAR2(50) NOT NULL,
+    end_estado           VARCHAR2(20) NOT NULL,
+    end_pais             VARCHAR2(30) NOT NULL,
+    end_is_residencial   CHAR(1) NOT NULL,
+    end_is_entrega       CHAR(1) NOT NULL,
+    end_is_cobranca      CHAR(1) NOT NULL,
+    end_observacoes      VARCHAR2(50),
+    end_cli_id           NUMBER(6) NOT NULL
 )
 LOGGING;
 
 ALTER TABLE enderecos ADD CONSTRAINT pk_end PRIMARY KEY ( end_id );
 
-CREATE TABLE generos (
-    gen_id   NUMBER(2) NOT NULL,
-    gen_nome VARCHAR2(20) NOT NULL
-)
-LOGGING;
-
-ALTER TABLE generos ADD CONSTRAINT pk_gen PRIMARY KEY ( gen_id );
-
-CREATE TABLE produtos (
-    prd_id     unknown 
+CREATE TABLE log_transacao (
+    data_hora           unknown 
 --  ERROR: Datatype UNKNOWN is not allowed 
     ,
-    prd_aut_id unknown 
+    usuario_responsavel unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    dados_alterados     unknown 
 --  ERROR: Datatype UNKNOWN is not allowed 
 
 )
 LOGGING;
+
+CREATE TABLE quadrinhos (
+    qua_id                 NUMBER(6) NOT NULL,
+    qua_ano                unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_titulo             unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_editora            unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_edicao             unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_isbn               unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_numero_paginas     unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_sinopse            unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_altura             unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_largura            unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_peso               unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_profundidade       unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_grupo_precificacao unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_codigo_barras      unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+    ,
+    qua_is_ativo           unknown 
+--  ERROR: Datatype UNKNOWN is not allowed 
+
+)
+LOGGING;
+
+ALTER TABLE quadrinhos ADD CONSTRAINT quadrinhos_pk PRIMARY KEY ( qua_id );
 
 CREATE TABLE telefones (
     tel_id     NUMBER(6) NOT NULL,
     tel_ddd    CHAR(2) NOT NULL,
     tel_numero CHAR(9) NOT NULL,
-    tel_tpt_id NUMBER(2) NOT NULL,
-    tel_cli_id NUMBER(6) NOT NULL
+    tel_tipo   VARCHAR2(20) NOT NULL
 )
 LOGGING;
 
 ALTER TABLE telefones ADD CONSTRAINT pk_tel PRIMARY KEY ( tel_id );
 
-CREATE TABLE tipos_logradouro (
-    tlo_id   NUMBER(2) NOT NULL,
-    tlo_nome VARCHAR2(20) NOT NULL
-)
-LOGGING;
-
-ALTER TABLE tipos_logradouro ADD CONSTRAINT pk_tpl PRIMARY KEY ( tlo_id );
-
-CREATE TABLE tipos_residencia (
-    tre_id   NUMBER(2) NOT NULL,
-    tre_nome VARCHAR2(20) NOT NULL
-)
-LOGGING;
-
-ALTER TABLE tipos_residencia ADD CONSTRAINT pk_tre PRIMARY KEY ( tre_id );
-
-CREATE TABLE tipos_telefone (
-    tpt_id   NUMBER(2) NOT NULL,
-    tpt_nome VARCHAR2(20) NOT NULL
-)
-LOGGING;
-
-ALTER TABLE tipos_telefone ADD CONSTRAINT pk_tpt PRIMARY KEY ( tpt_id );
-
-CREATE TABLE vendas 
-    
-    -- No Columns 
-
-LOGGING;
-
 ALTER TABLE clientes
-    ADD CONSTRAINT fk_cli_gen FOREIGN KEY ( cli_gen_id )
-        REFERENCES generos ( gen_id )
+    ADD CONSTRAINT fk_cli_tel FOREIGN KEY ( cli_tel_id )
+        REFERENCES telefones ( tel_id )
     NOT DEFERRABLE;
 
 ALTER TABLE cartoes_credito
-    ADD CONSTRAINT fk_cre_bcc FOREIGN KEY ( cre_bandeira_cartao )
+    ADD CONSTRAINT fk_cre_bcc FOREIGN KEY ( cre_bcc_id )
         REFERENCES bandeiras_cartao_credito ( bcc_id )
     NOT DEFERRABLE;
 
-ALTER TABLE cartoes_cliente
-    ADD CONSTRAINT fk_ctc_cli FOREIGN KEY ( ctc_cli_id )
+ALTER TABLE cartoes_credito
+    ADD CONSTRAINT fk_cre_cli FOREIGN KEY ( cre_cli_id )
         REFERENCES clientes ( cli_id )
-    NOT DEFERRABLE;
-
-ALTER TABLE cartoes_cliente
-    ADD CONSTRAINT fk_ctc_cre FOREIGN KEY ( ctc_cre_id )
-        REFERENCES cartoes_credito ( cre_id )
-    NOT DEFERRABLE;
-
-ALTER TABLE endereco_cobranca
-    ADD CONSTRAINT fk_eco_cli FOREIGN KEY ( eco_cli_id )
-        REFERENCES clientes ( cli_id )
-    NOT DEFERRABLE;
-
-ALTER TABLE endereco_cobranca
-    ADD CONSTRAINT fk_eco_end FOREIGN KEY ( eco_end_id )
-        REFERENCES enderecos ( end_id )
-    NOT DEFERRABLE;
-
-ALTER TABLE endereco_entrega
-    ADD CONSTRAINT fk_een_cli FOREIGN KEY ( een_cli_id )
-        REFERENCES clientes ( cli_id )
-    NOT DEFERRABLE;
-
-ALTER TABLE endereco_entrega
-    ADD CONSTRAINT fk_een_end FOREIGN KEY ( een_end_id )
-        REFERENCES enderecos ( end_id )
     NOT DEFERRABLE;
 
 ALTER TABLE enderecos
-    ADD CONSTRAINT fk_end_tlo FOREIGN KEY ( end_tlo_id )
-        REFERENCES tipos_logradouro ( tlo_id )
-    NOT DEFERRABLE;
-
-ALTER TABLE enderecos
-    ADD CONSTRAINT fk_end_tre FOREIGN KEY ( end_tre_id )
-        REFERENCES tipos_residencia ( tre_id )
-    NOT DEFERRABLE;
-
-ALTER TABLE telefones
-    ADD CONSTRAINT fk_tel_cli FOREIGN KEY ( tel_cli_id )
+    ADD CONSTRAINT fk_end_cli FOREIGN KEY ( end_cli_id )
         REFERENCES clientes ( cli_id )
-    NOT DEFERRABLE;
-
-ALTER TABLE telefones
-    ADD CONSTRAINT fk_tel_tpt FOREIGN KEY ( tel_tpt_id )
-        REFERENCES tipos_telefone ( tpt_id )
     NOT DEFERRABLE;
 
 
 
 -- Relatório do Resumo do Oracle SQL Developer Data Modeler: 
 -- 
--- CREATE TABLE                            14
+-- CREATE TABLE                             9
 -- CREATE INDEX                             0
--- ALTER TABLE                             24
+-- ALTER TABLE                             10
 -- CREATE VIEW                              0
 -- ALTER VIEW                               0
 -- CREATE PACKAGE                           0
@@ -248,5 +223,5 @@ ALTER TABLE telefones
 -- ORDS ENABLE SCHEMA                       0
 -- ORDS ENABLE OBJECT                       0
 -- 
--- ERRORS                                   2
+-- ERRORS                                  21
 -- WARNINGS                                 0
