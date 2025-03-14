2b3f2b7a6b19c9ec7fe8b47443a383fb802daeb5
EthanHueh <nattanssouzapvz@gmail.com>
2025-03-14 12:32:16 -0300

    Insert das bandeiras de cartão de crédito

---
+++ src/main/resources/insert_bandeiras_cartao.sql
@@ -0,0 +1,5 @@
+INSERT INTO bandeiras_cartao_credito (bcc_nome) VALUES ('Mastercard');
+INSERT INTO bandeiras_cartao_credito (bcc_nome) VALUES ('Visa');
+INSERT INTO bandeiras_cartao_credito (bcc_nome) VALUES ('American Express');
+INSERT INTO bandeiras_cartao_credito (bcc_nome) VALUES ('Hipercard');
+INSERT INTO bandeiras_cartao_credito (bcc_nome) VALUES ('Elo');
