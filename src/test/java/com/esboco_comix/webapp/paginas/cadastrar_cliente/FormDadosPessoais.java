8f04311514ddaffae1d7a3d14285bb4567b4fe3c
EthanHueh <nattanssouzapvz@gmail.com>
2025-03-16 22:55:30 -0300

    test: Agora a dt nascimento do cliente é inserida corretamente

--- src/test/java/com/esboco_comix/webapp/paginas/cadastrar_cliente/FormDadosPessoais.java
+++ src/test/java/com/esboco_comix/webapp/paginas/cadastrar_cliente/FormDadosPessoais.java
@@ -1,6 +1,7 @@
 package com.esboco_comix.webapp.paginas.cadastrar_cliente;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -9,10 +10,14 @@ import com.esboco_comix.model.entidades.Cliente;
 
 public class FormDadosPessoais {
 
+    private WebDriver driver;
     private WebElement form;
-
+    private WebElement campoData;
+        
     public FormDadosPessoais(WebDriver webDriver){
+        this.driver = webDriver;
         this.form = webDriver.findElement(By.id("cadastrar-dados-pessoais"));
+        this.campoData = form.findElement(By.name("dataNascimento"));
     }
 
     public void preencherInput(String nome, String valorInput){
@@ -28,7 +33,9 @@ public class FormDadosPessoais {
         preencherInput("tipoTelefone", c.getTelefone().getTipo());
         preencherInput("ddd", c.getTelefone().getDdd());
         preencherInput("numero", c.getTelefone().getNumero());
-        preencherInput("dataNascimento", String.valueOf(c.getDataNascimento()));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].value = '2013-02-03';", this.campoData);
 
         preencherInput("senhaNova", pedido.getSenhaNova());
         preencherInput("senhaConfirmacao", pedido.getSenhaConfirmacao());
