228f1fd5196c871437978a3c1e8006a5a17fd726
EthanHueh <nattanssouzapvz@gmail.com>
2025-04-05 21:51:24 -0300

    feat: agora a página de anúncio é renderizada com o quadrinho selecionado

--- src/main/webapp/js/api/apiQuadrinho.js
+++ src/main/webapp/js/api/apiQuadrinho.js
@@ -8,3 +8,13 @@ export async function consultarTodosQuadrinhos(){
         console.error('Erro buscando dados:', error);
     }
 }
+
+export async function retornarQuadrinho(id) {
+    try {
+        const response = await fetch("/quadrinho?id="+id);
+        return await response.json();
+
+    } catch (error) {
+        console.error('Erro buscando dados:', error);
+    }
+}
---
+++ src/main/webapp/paginas/cliente/anuncio/js/anuncio.js
@@ -0,0 +1,60 @@
+import { retornarQuadrinho } from "/js/api/apiQuadrinho.js";
+
+const uRLSearchParams = new URLSearchParams(window.location.search);
+const quadrinho = await retornarQuadrinho(uRLSearchParams.get('id'));
+
+const img = document.createElement('img');
+img.className = 'img-fluid rounded-start';
+img.alt = 'Imagem do produto';
+img.src = quadrinho.urlImagem;
+
+document.getElementById('secao-imagem').append(img);
+
+document.getElementById('header-produto').innerHTML = `
+    <h1 class="card-title">${quadrinho.titulo}</h1>
+    <p class="card-text fs-4 text-success">R$ ${quadrinho.preco}</p>
+`
+
+const corpoTabela = document.createElement('tbody');
+corpoTabela.innerHTML = `
+    <tbody>
+        <tr>
+            <td>Ano: ${quadrinho.ano}</td>
+        </tr>
+        <tr>
+            <td>Autor: ${quadrinho.autor}</td>
+        </tr>
+        <tr>
+            <td>Título: ${quadrinho.titulo}</td>
+        </tr>
+        <tr>
+            <td>Editora: ${quadrinho.editora}</td>
+        </tr>
+        <tr>
+            <td>Edição: ${quadrinho.edicao}</td>
+        </tr>
+        <tr>
+            <td>ISBN: ${quadrinho.isbn}</td>
+        </tr>
+        <tr>
+            <td>Número de Páginas: ${quadrinho.numeroPaginas}</td>
+        </tr>
+        <tr>
+            <td>Sinopse: ${quadrinho.sinopse}</td>
+        </tr>
+        <tr>
+            <td>Dimensões: ${quadrinho.altura}cm X ${quadrinho.largura}cm X ${quadrinho.profundidade}cm</td>
+        </tr>
+        <tr>
+            <td>Peso: ${quadrinho.peso}g</td>
+        </tr>
+        <tr>
+            <td>Código de Barras: ${quadrinho.codigoBarras}</td>
+        </tr>
+        <tr>
+            <td>Categorias: ${quadrinho.categoria}</td>
+        </tr>
+    </tbody>
+`
+
+document.getElementById('tabela-info').append(corpoTabela);
