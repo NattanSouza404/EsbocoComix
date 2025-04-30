37f101dc040da89031815892435aecce7ab1b554
EthanHueh <nattanssouzapvz@gmail.com>
2025-04-30 12:56:03 -0300

    refactor: deleção do pseudologin na página de login (já é possível usar a página de clientes para realizar o pseudologin)

--- src/main/webapp/paginas/login/js/login.js
+++
@@ -1,7 +0,0 @@
-import ModalLogin from "./ModalLogin.js";
-import { retornarAllClientes } from "/js/api/apiCliente.js";
-
-const clientes = await retornarAllClientes();
-const modalLogin = new ModalLogin();
-
-modalLogin.preencherSelectCliente(clientes);
