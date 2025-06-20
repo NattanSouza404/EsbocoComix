ed4d58ae22ce9c07aed74fd00c6299d6b9ccaeb1
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-06-20 13:30:50 -0300

    feat e refactor: agora as consultas do banco api de IA são retiradas da API principal do sistema

--- src/main/chatbot/app/config/config.py
+++ src/main/chatbot/app/config/config.py
@@ -4,9 +4,3 @@ config = configparser.ConfigParser()
 config.read('../resources/config.properties')
 
 api_key = config['DEFAULT']['api.key']
-
-db_host = config['DEFAULT']['database.host']
-db_port = config['DEFAULT']['database.port']
-db_database = config['DEFAULT']['database.database']
-db_user = config['DEFAULT']['database.user']
-db_password = config['DEFAULT']['database.password']
