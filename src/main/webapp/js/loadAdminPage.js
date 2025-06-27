511fad0128eb8d9f874d90cfaeaa2778f712a49c
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-06-27 16:59:17 -0300

    feat: ajuste de estilo na navegação das páginas do admin

--- src/main/webapp/js/loadAdminPage.js
+++ src/main/webapp/js/loadAdminPage.js
@@ -3,6 +3,12 @@ import { AdminContainerNav } from "/js/componentes/adminContainerNav.js";
 if (document.getElementById("main-nav")){
     document.getElementById("main-nav").className = "navbar navbar-expand-lg navbar-light";
     document.getElementById("main-nav").append(new AdminContainerNav());
+
+    document.querySelectorAll(".nav-link").forEach( link => {
+        if (link.href === window.location.href){
+            link.className = 'nav-link active';
+        }
+    });
 }
 
 const script = document.createElement('script');
