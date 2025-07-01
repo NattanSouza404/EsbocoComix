0f6f4dd3c03c9656f0e65f0d818604a9092ce7d2
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-07-01 20:54:01 -0300

    feat e refactor: remodelagem dos fluxos do crud do cliente

--- src/test/java/com/esboco_comix/webapp/casos_de_uso/CadastrarCliente.java
+++ src/test/java/com/esboco_comix/webapp/casos_de_uso/CadastrarCliente.java
@@ -1,33 +1,75 @@
 package com.esboco_comix.webapp.casos_de_uso;
 
+import com.esboco_comix.dto.AlterarSenhaDTO;
 import com.esboco_comix.dto.CadastrarClienteDTO;
-import com.esboco_comix.model.entidades.Endereco;
+import com.esboco_comix.model.entidades.Cliente;
+import com.esboco_comix.webapp.paginas.admin.clientes.PaginaClientes;
 import com.esboco_comix.webapp.paginas.cliente.cadastrar.PaginaCadastrar;
+import com.esboco_comix.webapp.paginas.cliente.conta.ModaisConta;
+import com.esboco_comix.webapp.paginas.cliente.conta.PaginaConta;
+import com.esboco_comix.webapp.paginas.cliente.conta.SecoesConta;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 public class CadastrarCliente {
-    private PaginaCadastrar paginaCadastrar;
+    private final PaginaCadastrar paginaCadastrar;
+    private final PaginaClientes paginaClientes;
+    private final PaginaConta paginaConta;
 
     public CadastrarCliente(WebDriver driver, WebDriverWait wait) throws Exception {
         paginaCadastrar = new PaginaCadastrar(driver, wait);
+        paginaClientes = new PaginaClientes(driver, wait);
+        paginaConta = new PaginaConta(driver, wait);
     }
 
-    public void abrir() throws InterruptedException {
+    public void cadastrarCliente(CadastrarClienteDTO cadastro) throws InterruptedException {
         paginaCadastrar.abrir();
-    }
-
-    public void preencherCliente(CadastrarClienteDTO dto) throws InterruptedException {
-        paginaCadastrar.preencherCliente(dto);
-    }
-
-    public void preencherEnderecos(List<Endereco> enderecos) throws InterruptedException {
-        paginaCadastrar.preencherEnderecos(enderecos);
-    }
-
-    public void enviarCadastro() throws InterruptedException {
+        paginaCadastrar.preencherCliente(cadastro);
+        paginaCadastrar.preencherEnderecos(cadastro.getEnderecos());
         paginaCadastrar.enviarCadastro();
     }
+
+    public void mostrarPaginaConta() throws InterruptedException {
+        paginaClientes.abrir();
+        paginaClientes.logarComoNovoCliente();
+        paginaConta.trocarSecao(SecoesConta.ENDERECO);
+        paginaConta.trocarSecao(SecoesConta.CARTAO_CREDITO);
+        paginaConta.trocarSecao(SecoesConta.CUPOM);
+        paginaConta.trocarSecao(SecoesConta.DADOS_PESSOAIS);
+    }
+
+    public void editarNovoCliente(Cliente c) throws InterruptedException {
+        paginaClientes.abrir();
+        paginaClientes.logarComoNovoCliente();
+        ModaisConta modal = ModaisConta.ALTERAR_DADOS_PESSOAIS;
+
+        paginaConta.trocarSecao(SecoesConta.DADOS_PESSOAIS);
+        paginaConta.abrirModal(modal);
+
+        paginaConta.preencherInputModal(modal, "nome", c.getNome());
+        paginaConta.preencherInputDataModal(modal , "dataNascimento", c.getDataNascimento());
+        paginaConta.enviar(modal);
+    }
+
+    public void editarSenhaCliente(AlterarSenhaDTO dto) throws InterruptedException {
+        paginaClientes.abrir();
+        paginaClientes.logarComoNovoCliente();
+
+        ModaisConta modal = ModaisConta.ALTERAR_SENHA;
+
+        paginaConta.trocarSecao(SecoesConta.DADOS_PESSOAIS);
+        paginaConta.abrirModal(modal);
+
+        paginaConta.preencherInputModal(modal, "senhaAntiga", dto.getSenhaAntiga());
+        paginaConta.preencherInputModal(modal, "senhaNova", dto.getSenhaNova());
+        paginaConta.preencherInputModal(modal, "senhaConfirmacao", dto.getSenhaConfirmacao());
+
+        paginaConta.enviar(modal);
+    }
+
+    public void inativarNovoCliente() throws InterruptedException {
+        paginaClientes.abrir();
+        paginaClientes.inativarNovoCliente();
+    }
+
 }
