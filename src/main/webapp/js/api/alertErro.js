ba4d01a3d748b045289763acee5576d3341c29ef
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-07-30 19:52:29 -0300

    refactor: criação do método estourarErroAPI no frontend

--- src/main/webapp/js/api/alertErro.js
+++ src/main/webapp/js/api/alertErro.js
@@ -2,3 +2,8 @@ export function alertarErro(error){
     alert(error.message);
     console.error(error.message);
 }
+
+export async function estourarErroAPI(resposta){
+    const json = await resposta.json();
+    throw new Error(json.erro);
+}
