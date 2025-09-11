83d0b6fac93bfcd15073ce95f6e71f4b25b47a60
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-09-10 22:39:49 -0300

    fix: adição da validação na inserção e atualização de cartões e endereços

--- src/main/java/com/esboco_comix/service/impl/CartaoCreditoService.java
+++ src/main/java/com/esboco_comix/service/impl/CartaoCreditoService.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import com.esboco_comix.dao.impl.cartao_credito.CartaoCreditoDAO;
 import com.esboco_comix.model.entidades.CartaoCredito;
+import com.esboco_comix.validador.impl.cartao_credito.CartaoCreditoValidador;
 
 public class CartaoCreditoService {
     private CartaoCreditoDAO cartaoCreditoDAO = new CartaoCreditoDAO();
+    private CartaoCreditoValidador cartaoCreditoValidador = new CartaoCreditoValidador();
 
     public CartaoCredito inserir(CartaoCredito c) {
+        // TODO: ao inserir um cliente, acabamos verificando o cartão de crédito 2 vezes
+        cartaoCreditoValidador.validar(c);
         return cartaoCreditoDAO.inserir(c);
     }
 
@@ -21,6 +25,7 @@ public class CartaoCreditoService {
     }
 
     public CartaoCredito atualizar(CartaoCredito c){
+        cartaoCreditoValidador.validar(c);
         return cartaoCreditoDAO.atualizar(c);
     }
 
