afa2809905bc4d2bf532f9db3b53bb0f8c8fd384
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-11-12 17:59:32 -0300

    feat: adição de classes de validação

---
+++ src/main/java/com/esboco_comix/validador/impl/estoque/EntradaEstoqueValidador.java
@@ -0,0 +1,15 @@
+package com.esboco_comix.validador.impl.estoque;
+
+import com.esboco_comix.model.entidades.EntradaEstoque;
+import com.esboco_comix.validador.AbstractValidador;
+import com.esboco_comix.validador.IValidador;
+
+public class EntradaEstoqueValidador extends AbstractValidador implements IValidador<EntradaEstoque>{
+
+    @Override
+    public void validar(EntradaEstoque entradaEstoque) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validar'");
+    }
+    
+}
