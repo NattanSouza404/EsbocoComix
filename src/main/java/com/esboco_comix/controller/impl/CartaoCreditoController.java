a5225ea31e0463991cb5bf112ca2e62add45e907
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-01-16 22:06:59 -0300

    refactor: grandes refatorações nos endpoints nas controllers

--- src/main/java/com/esboco_comix/controller/impl/CartaoCreditoController.java
+++ src/main/java/com/esboco_comix/controller/impl/CartaoCreditoController.java
@@ -1,77 +1,104 @@
 package com.esboco_comix.controller.impl;
 
 import com.esboco_comix.controller.utils.AbstractController;
+import com.esboco_comix.controller.utils.Router;
 import com.esboco_comix.model.entidades.CartaoCredito;
 import com.esboco_comix.service.impl.CartaoCreditoService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class CartaoCreditoController extends AbstractController {
 
     private final CartaoCreditoService cartaoCreditoService = new CartaoCreditoService();
 
+    private final Router rotasGet = new Router(
+        Map.of(
+            "/por-id-cliente", this::consultarPorIdCliente
+        )
+    );
+
+    private final Router rotasPost = new Router(
+        Map.of(
+            "", this::adicionar
+        )
+    );
+
+    private final Router rotasPut = new Router(
+        Map.of(
+            "", this::atualizar
+        )
+    );
+
+    private final Router rotasDelete = new Router(
+        Map.of(
+            "", this::deletar
+        )
+    );
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-
-            String parametroIdCliente = req.getParameter("idcliente");
-
-            if (parametroIdCliente != null){
-                int id = Integer.parseInt(parametroIdCliente);
-
-                retornarRespostaJson(
-                    resp,
-                    cartaoCreditoService.consultarByIDCliente(id),
-                    HttpServletResponse.SC_OK
-                );
-
-            }
-
-        } catch (Exception e) {
-            estourarErro(resp, new Exception("Erro ao consultar cartões de crédito", e));
-        }
-
+        processar(req,
+            resp,
+            rotasGet,
+            HttpServletResponse.SC_OK,
+            "Erro ao consultar cartões de crédito"
+        );
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-            CartaoCredito cartaoCreditoToAdd = jsonToObject(req, CartaoCredito.class);
-            retornarRespostaJson(
-                resp,
-                cartaoCreditoService.inserir(cartaoCreditoToAdd),
-                HttpServletResponse.SC_CREATED
-            );
-        } catch (Exception e) {
-            estourarErro(resp, new Exception("Erro ao adicionar cartão de crédito", e));
-        }
+        processar(
+            req,
+            resp,
+            rotasPost,
+            HttpServletResponse.SC_CREATED,
+            "Erro ao adicionar cartão de crédito"
+        );
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        
-        try {
-            CartaoCredito cartaoCredito = jsonToObject(req, CartaoCredito.class);
-            retornarRespostaJson(
-                resp,
-                cartaoCreditoService.atualizar(cartaoCredito),
-                HttpServletResponse.SC_OK
-            );
-        } catch (Exception e) {
-            estourarErro(resp, new Exception("Erro ao atualizar cartão de crédito", e));
-        }
+        processar(
+            req,
+            resp,
+            rotasPut,
+            HttpServletResponse.SC_OK,
+            "Erro ao atualizar cartão de crédito"
+        );
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-            CartaoCredito cartaoCredito = jsonToObject(req, CartaoCredito.class);
-            cartaoCreditoService.deletar(cartaoCredito);
-            resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
-        } catch (Exception e) {
-            estourarErro(resp, new Exception("Erro ao deletar cartão de crédito", e));
-        }
+        processar(
+            req,
+            resp,
+            rotasDelete,
+            HttpServletResponse.SC_NO_CONTENT,
+            "Erro ao deletar cartão de crédito"
+        );
+    }
+
+    private Object consultarPorIdCliente(HttpServletRequest req) throws Exception {
+        int id = Integer.parseInt(requireParam(req, "id"));
+        return cartaoCreditoService.consultarByIDCliente(id);
+    }
+
+    private Object adicionar(HttpServletRequest req) throws Exception {
+        CartaoCredito cartaoCreditoToAdd = jsonToObject(req, CartaoCredito.class);
+        return cartaoCreditoService.inserir(cartaoCreditoToAdd);
+    }
+
+    private Object atualizar(HttpServletRequest req) throws Exception {
+        CartaoCredito cartaoCredito = jsonToObject(req, CartaoCredito.class);   
+        return cartaoCreditoService.atualizar(cartaoCredito);
+    }
+
+    private Object deletar(HttpServletRequest req) throws Exception {
+        CartaoCredito cartaoCredito = jsonToObject(req, CartaoCredito.class);
+        cartaoCreditoService.deletar(cartaoCredito);
+        return null;
     }
 }
--- src/main/java/com/esboco_comix/controller/impl/EstoqueController.java
+++ src/main/java/com/esboco_comix/controller/impl/EstoqueController.java
@@ -1,42 +1,58 @@
 package com.esboco_comix.controller.impl;
 
 import com.esboco_comix.controller.utils.AbstractController;
+import com.esboco_comix.controller.utils.Router;
 import com.esboco_comix.model.entidades.EntradaEstoque;
 import com.esboco_comix.service.impl.EstoqueService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class EstoqueController extends AbstractController {
     private final EstoqueService estoqueService = new EstoqueService();
 
+    private final Router rotasGet = new Router(
+        Map.of(
+            "", this::consultar
+        )
+    );
+
+    private final Router rotasPost = new Router(
+        Map.of(
+            "", this::adicionar
+        )
+    );
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-        try {
-            retornarRespostaJson(
-                resp,
-                estoqueService.consultarEntradasEstoque(),
-                HttpServletResponse.SC_OK
-            );
-
-        } catch (Exception e) {
-            estourarErro(resp, new Exception("Erro ao consultar estoque", e));
-        }
+        processar(
+            req,
+            resp, 
+            rotasGet, 
+            HttpServletResponse.SC_OK,
+            "Erro ao consultar estoque"
+        );
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-            EntradaEstoque entradaEstoque = jsonToObject(req, EntradaEstoque.class);
-            retornarRespostaJson(
-                resp,
-                estoqueService.inserir(entradaEstoque),
-                HttpServletResponse.SC_CREATED
-            );
-        } catch (Exception e) {
-            estourarErro(resp, new Exception("Erro ao adicionar estoque", e));
-        }
+        processar(
+            req,
+            resp, 
+            rotasPost, 
+            HttpServletResponse.SC_CREATED,
+            "Erro ao adicionar estoque"
+        );
+    }
+
+    private Object consultar(HttpServletRequest req) throws Exception {
+        return estoqueService.consultarEntradasEstoque();
+    }
+
+    private Object adicionar(HttpServletRequest req) throws Exception {
+        EntradaEstoque entradaEstoque = jsonToObject(req, EntradaEstoque.class);
+        return estoqueService.inserir(entradaEstoque);
     }
 }
