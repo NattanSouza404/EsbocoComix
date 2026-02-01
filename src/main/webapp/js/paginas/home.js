84407b14bae59ad50552949516b91f5e95fc4378
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-02-01 12:08:26 -0300

    refactor: página home

--- src/main/webapp/js/paginas/home.js
+++ src/main/webapp/js/paginas/home.js
@@ -1,86 +1,102 @@
-import { consultarTodosQuadrinhos } from "@api/quadrinho.api.js";
-import { CartaoProduto } from "@componentes/quadrinho/CartaoProduto.js";
-import { consultarTodasCategorias, filtrarTodosQuadrinhos } from "@api/quadrinho.api.js";
 import { alertarErro } from "@api/alertErro.js";
+import { consultarTodasCategorias, consultarTodosQuadrinhos, filtrarTodosQuadrinhos } from "@api/quadrinho.api.js";
+import { CarrosselHome } from "@componentes/layout/CarrosselHome.js";
+import { CartaoProduto } from "@componentes/quadrinho/CartaoProduto.js";
+import { CheckCategoria } from "@componentes/quadrinho/CheckCategoria.js";
 import { formToObject } from "../script.js";
+import { InputMaximoPaginas } from "@componentes/quadrinho/InputMaximoPaginas.js";
+
+const getElementos = () => {
+    return {
+        containerCarrossel: document.getElementById('container-carrossel'),
+        containerProdutos: document.getElementById('container-produtos'),
+        btnBuscarQuadrinho: document.getElementById('btn-buscar-quadrinho'),
+        buscaQuadrinhos: document.getElementById('busca-quadrinhos'),
+        fieldSetCategorias: document.getElementById('fieldset-categorias'),
+        inputMaximoPaginas: document.getElementById("input-maximo-paginas"),
+        checksCategorias: document.querySelectorAll('[name = "categorias"]')
+    };
+}
 
 export async function initPagina(){
-    let quadrinhos;
-    let categorias;
-
     try {
-        quadrinhos = await consultarTodosQuadrinhos();
-        categorias = await consultarTodasCategorias();
-    } catch (error){
-        alertarErro(error);
-    }
+        getElementos().containerCarrossel.append(CarrosselHome());
 
-    quadrinhos.forEach(quadrinho => {
-        document.getElementById('container-produtos').append(new CartaoProduto(quadrinho))
-    });
+        const quadrinhos = await consultarTodosQuadrinhos();
+        const categorias = await consultarTodasCategorias();
+        
+        quadrinhos.forEach(quadrinho => {
+            getElementos().containerProdutos.append(new CartaoProduto(quadrinho))
+        });
 
-    categorias.forEach(c => {
-        document.getElementById("fieldset-categorias").insertAdjacentHTML(
-            "beforeend", 
-            `
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" name="categorias" id="cat${c.nome.replaceAll(" ", "")}" value="${c.nome}">
-                <label class="form-check-label" for="cat${c.nome.replaceAll(" ", "")}">${c.nome}</label>
-            </div>
-            `
-        );
-    });           
+        categorias.forEach(c => {
+            getElementos().fieldSetCategorias.append(CheckCategoria(c));
+        });
 
-    document.getElementById('btn-buscar-quadrinho').onclick = async () => {
-        pesquisarQuadrinhos();
-    }
-
-    document.getElementById('busca-quadrinhos').addEventListener(
-        "keypress", 
-        e => {
-            if (e.key === "Enter") {
-                e.preventDefault();
-                pesquisarQuadrinhos();
-            }
+        getElementos().btnBuscarQuadrinho.onclick = async () => {
+            pesquisarQuadrinhos();
         }
-    );
 
-    document.querySelector('[name = "numeroPaginas"]').addEventListener('change', (e) => {
-        const valor = /** @type {HTMLInputElement} */
-            (document.querySelector('[name = "numeroPaginas"]')).value;
-        document.getElementById('input-maximo-paginas').textContent = `Máximo de Páginas = ${valor}`;
-    });
+        getElementos().inputMaximoPaginas.append(InputMaximoPaginas());
 
-    async function pesquisarQuadrinhos(){
-        const filtro = formToObject((document.getElementById('busca-quadrinhos')));
-        const categorias = [];
-        document.querySelectorAll('[name = "categorias"').forEach(
-            (/** @type {HTMLInputElement} */
-                input
-            ) => {
-                if (input.checked){
-                    categorias.push(input.value);
+        getElementos().buscaQuadrinhos.addEventListener(
+            "keypress", 
+            e => {
+                if (e.key === "Enter") {
+                    e.preventDefault();
+                    pesquisarQuadrinhos();
                 }
             }
         );
 
-        filtro.categorias = categorias;
+    } catch (error){
+        alertarErro(error);
+    }
+}
 
-        let quadrinhos;
-        try {
-            quadrinhos = await filtrarTodosQuadrinhos(filtro);
-        } catch (error){
-            alertarErro(error);
-        }
+async function pesquisarQuadrinhos(){
+    try {
+        const form = getElementos().buscaQuadrinhos;
+
+        const dadosFormulario = formToObject(form);
+        const categorias = listarCategoriasSelecionadas();
+
+        const filtro = {
+            ...dadosFormulario,
+            categorias
+        };
+
+        const quadrinhos = await filtrarTodosQuadrinhos(filtro);
 
         if (!Array.isArray(quadrinhos) || quadrinhos.length === 0){
-            document.getElementById('container-produtos').innerHTML = `<h1 class="text-center">Nenhum item encontrado!</h1>`; 
+            getElementos().containerProdutos.innerHTML = /* html */
+                `<h1 class="text-center">Nenhum item encontrado!</h1>`; 
+            
             return;
         }
 
-        document.getElementById('container-produtos').innerHTML = "";  
+        getElementos().containerProdutos.innerHTML = "";  
         quadrinhos.forEach(quadrinho => {
-            document.getElementById('container-produtos').append(new CartaoProduto(quadrinho))
+            getElementos().containerProdutos.append(new CartaoProduto(quadrinho))
         });
+
+    } catch (error){
+        alertarErro(error);
     }
+}
+
+function listarCategoriasSelecionadas(){
+    const categoriasSelecionadas = [];
+
+    getElementos().checksCategorias.forEach(
+        (/** @type {HTMLInputElement} */
+            input
+        ) => {
+            if (input.checked){
+                categoriasSelecionadas.push(input.value);
+            }
+        }
+    );
+
+    return categoriasSelecionadas;
 }
