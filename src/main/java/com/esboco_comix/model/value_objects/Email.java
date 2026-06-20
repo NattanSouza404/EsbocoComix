927bc606fa0f50dc823a80bc37851085e8aee428
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-06-20 18:35:32 -0300

    feat: criando value object Email

---
+++ src/main/java/com/esboco_comix/model/value_objects/Email.java
@@ -0,0 +1,34 @@
+package com.esboco_comix.model.value_objects;
+
+import java.util.regex.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public record Email(
+    String valor
+) {
+
+    private static final Pattern REGEX_EMAIL = Pattern.compile(
+        "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+    );
+
+    @JsonCreator
+    public Email {
+        if (valor == null || valor.isBlank()) {
+            throw new IllegalArgumentException("Email não pode ser nulo ou vazio!");
+        }
+
+        valor = valor.trim();
+
+        if (!REGEX_EMAIL.matcher(valor).matches()) {
+            throw new IllegalArgumentException("Email inválido!");
+        }
+    }
+
+    @JsonValue
+    public String valor() {
+        return valor;
+    }
+}
+
