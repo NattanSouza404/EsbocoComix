0f412cc89d3ed3484a93cc6a863782dddb78e9c3
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-06-20 18:19:54 -0300

    test: adicionadno testes unitários para Pedido

---
+++ src/test/java/com/esboco_comix/backend/model/entidades/PedidoTest.java
@@ -0,0 +1,44 @@
+package com.esboco_comix.backend.model.entidades;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.esboco_comix.model.entidades.CartaoCreditoPedido;
+import com.esboco_comix.model.entidades.CupomPedido;
+import com.esboco_comix.model.entidades.Pedido;
+
+public class PedidoTest {
+
+    @Test
+    public void validarPedidoComFormaDePagamentoValida() {
+        Pedido pedido = new Pedido();
+
+        List<CartaoCreditoPedido> cartoesCreditoPedido = new ArrayList<>();
+        cartoesCreditoPedido.add(new CartaoCreditoPedido());
+
+        List<CupomPedido> cuponsPedido = new ArrayList<>();
+        cuponsPedido.add(new CupomPedido());
+
+        pedido.setCartoesCreditoPedido(cartoesCreditoPedido);
+        pedido.setCuponsPedido(cuponsPedido);
+        pedido.validarFormaPagamento();
+    }
+
+    @Test
+    public void validarPedidoSemFormaDePagamento() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                Pedido pedido = new Pedido();
+        
+                pedido.setCartoesCreditoPedido(new ArrayList<>());
+                pedido.setCuponsPedido(new ArrayList<>());
+                pedido.validarFormaPagamento();
+            }
+        );
+    }
+}
