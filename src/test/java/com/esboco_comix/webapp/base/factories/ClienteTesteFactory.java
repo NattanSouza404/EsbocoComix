b3fc3eaba8dd19eb6f7518f8b8a41bde5088510f
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-06-21 13:38:59 -0300

    test: criando e atualizando testes

--- src/test/java/com/esboco_comix/webapp/base/factories/ClienteTesteFactory.java
+++ src/test/java/com/esboco_comix/webapp/base/factories/ClienteTesteFactory.java
@@ -1,11 +1,11 @@
 package com.esboco_comix.webapp.base.factories;
 
 import com.esboco_comix.model.entidades.Cliente;
-import com.esboco_comix.model.entidades.Telefone;
 import com.esboco_comix.model.enuns.Genero;
 import com.esboco_comix.model.enuns.TipoTelefone;
 import com.esboco_comix.model.value_objects.Cpf;
 import com.esboco_comix.model.value_objects.Email;
+import com.esboco_comix.model.value_objects.Telefone;
 
 import java.time.LocalDate;
 
@@ -19,11 +19,9 @@ public class ClienteTesteFactory {
         cliente.setDataNascimento(LocalDate.of(2013, 2, 3));
         cliente.setGenero(Genero.MASCULINO);
 
-        Telefone t = new Telefone();
-        t.setDdd("11");
-        t.setNumero("99999999");
-        t.setTipo(TipoTelefone.CELULAR);
-        cliente.setTelefone(t);
+        cliente.setTelefone(
+            new Telefone("11", "99999999", TipoTelefone.CELULAR)
+        );
 
         return cliente;
     }
--- src/test/java/com/esboco_comix/webapp/paginas/cliente/cadastrar/PaginaCadastrar.java
+++ src/test/java/com/esboco_comix/webapp/paginas/cliente/cadastrar/PaginaCadastrar.java
@@ -28,13 +28,13 @@ public class PaginaCadastrar extends AbstractPagina {
         form.findElement(By.name("nome")).sendKeys(c.getNome());
         form.findElement(By.name("cpf")).sendKeys(c.getCpf().valor());
         form.findElement(By.name("email")).sendKeys(c.getEmail().valor());
-        form.findElement(By.name("tipoTelefone")).sendKeys(c.getTelefone().getTipo().name());
-        form.findElement(By.name("ddd")).sendKeys(c.getTelefone().getDdd());
-        form.findElement(By.name("numero")).sendKeys(c.getTelefone().getNumero());
+        form.findElement(By.name("tipoTelefone")).sendKeys(c.getTelefone().tipo().name());
+        form.findElement(By.name("ddd")).sendKeys(c.getTelefone().ddd());
+        form.findElement(By.name("numero")).sendKeys(c.getTelefone().numero());
         form.findElement(By.name("senhaNova")).sendKeys(pedido.getSenhaNova());
         form.findElement(By.name("senhaConfirmacao")).sendKeys(pedido.getSenhaConfirmacao());
 
-        preencherInputSelect(form, "tipoTelefone", c.getTelefone().getTipo().name());
+        preencherInputSelect(form, "tipoTelefone", c.getTelefone().tipo().name());
         preencherInput(form, "dataNascimento", c.getDataNascimento());
 
         sleep();
