61cc9c8762ad7995397f488f3930faa7e528dceb
NattanSouza404 <nattanssouzapvz@gmail.com>
2026-07-09 16:47:34 -0300

    refactor: criando CategoriaDTO

--- src/main/java/com/esboco_comix/service/impl/QuadrinhoService.java
+++ src/main/java/com/esboco_comix/service/impl/QuadrinhoService.java
@@ -1,15 +1,19 @@
 package com.esboco_comix.service.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.esboco_comix.dao.impl.quadrinho.QuadrinhoDAO;
+import com.esboco_comix.dto.CategoriaDTO;
 import com.esboco_comix.dto.FiltrarQuadrinhoDTO;
 import com.esboco_comix.dto.QuadrinhoDTO;
-import com.esboco_comix.model.entidades.Categoria;
+import com.esboco_comix.mapper.CategoriaMapper;
 
 public class QuadrinhoService {
 
     private final QuadrinhoDAO quadrinhoDAO = new QuadrinhoDAO();
+
+    private final CategoriaMapper categoriaMapper = new CategoriaMapper();
 
     public QuadrinhoDTO consultarByID(int id) {
         return quadrinhoDAO.consultarByID(id);
@@ -19,8 +23,10 @@ public class QuadrinhoService {
         return quadrinhoDAO.consultarTodos();
     }
 
-    public List<Categoria> consultarTodasCategorias() {
-        return quadrinhoDAO.consultarTodasCategorias();
+    public List<CategoriaDTO> consultarTodasCategorias() {     
+        return quadrinhoDAO.consultarTodasCategorias().stream()
+            .map(categoriaMapper::toDTO)
+        .collect(Collectors.toList());
     }
 
     public List<QuadrinhoDTO> filtrarTodos(FiltrarQuadrinhoDTO filtro) {
